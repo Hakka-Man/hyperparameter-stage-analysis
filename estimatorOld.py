26bf7634f0ed8130e0467b8f306a90e2cff59e02
Alan Hou <alanhou911222@gmail.com>
2022-04-09 15:21:36 -0400

    bring back pool

--- estimatorOld.py
+++ estimatorOld.py
@@ -10,34 +10,6 @@ from sklearn.model_selection import train_test_split
 import warnings
 import pickle
 from multiprocessing import Pool
-# import mariadb
-import sys
-import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-### ENV CONSTANT
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_USER = os.getenv('DB_USER')
-
-### Connect To DB
-# try:
-#     conn = mariadb.connect(
-#         user=DB_USER,
-#         password=DB_PASSWORD,
-#         host=DB_HOST,
-#         port=3306,
-#         database="stock",
-#         autocommit=True
-#     )
-# except mariadb.Error as e:
-#     print(f"Error connecting to MariaDB Platform: {e}")
-#     sys.exit(1)
-# cur = conn.cursor()
-
-
 
 ## Warning Statements
 warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning) 
@@ -156,16 +128,12 @@ resultFile.write("test "+str(test)+"\n")
 resultFile.close()
 
 pop = toolbox.population(n=512)
-for ind in pop:
-    paramStr = ' '.join(map(str, ind[0]))
-    print(paramStr)
-    # cur.execute("INSERT INTO Params (param,result) VALUES (?, NULL)", (paramStr,))
 # Evaluate the entire population
 # here
 pool = Pool()
 # tempResult = pool.map(toolbox.evaluate, pop)
 fitnesses = pool.map(toolbox.evaluate, pop)
-# print(fitnesses)
+print(fitnesses)
 # here
 # pool.close()
 for ind, fit in zip(pop, fitnesses):
